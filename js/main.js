21d6df28b5a66a0ae4f495441cb33901b35bbff9
Andrea Ligorio <andrea85kro@gmail.com>
2021-03-12 17:00:17 +0100

    Esercizi completati

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ var pesoTot = 0;
 for (var i = 0; i < zucchine.length; i++) {
     pesoTot += zucchine[i]["peso"];
 }
-
+console.warn("ESERCIZIO 1");
 console.log("Peso totale: " + pesoTot + " grammi");
 
 /* 2)
@@ -83,6 +83,8 @@ for (var i = 0; i < zucchine.length; i++) {
         zucchineOver15.push(zucchine[i]);
     }
 }
+
+console.warn("ESERCIZIO 2");
 console.log(zucchineUnder15);
 console.log(zucchineOver15);
 
@@ -105,6 +107,7 @@ Scrivi una funzione che accetti una stringa come
 argomento e la ritorni girata (es. Ciao -> oaiC) */
 
 var word = reverseString("Ciao");
+console.warn("ESERCIZIO 3");
 console.log(word);
 
 function reverseString(word) {
@@ -115,5 +118,117 @@ function reverseString(word) {
     return reverseWord
 }
 
-});
 
+/* 4)
+Generatore di “nomi cognomi” casuali: prendendo una lista
+di nomi e una lista di cognomi, Gatsby vuole generare una
+falsa lista di 3 invitati. */
+
+var nameList = ["Marco", "Luca", "Andrea", "Paolo", "Matteo", "Giorgio"];
+var surnameList = ["Rossi", "Verdi", "Marrone", "Bianchi", "Neri", "Viola"];
+
+var fakeListName = [];          //Ho dovuto creare 2 variabili d'appoggio perchè il metodo includes non funzionava nel momento
+var fakeListSurname = [];       //che controllavo se un nome era già stato utilizzato (es. "Marco Neri" includes non rilevava "Marco")
+var fakeList = [];
+
+while (fakeList.length < 3) {
+    var fakeName = nameGenerator(nameList);
+    var fakeSurname = nameGenerator(surnameList);
+    if (!fakeListName.includes(fakeName) && !fakeListSurname.includes(fakeSurname)) {
+        fakeListName.push(fakeName);
+        fakeListSurname.push(fakeSurname);
+        fakeList.push(fakeName + " " + fakeSurname);
+    }
+}
+
+console.warn("ESERCIZIO 4");
+console.log(fakeList);
+
+function nameGenerator(nameList) {
+    var i = Math.floor(Math.random() * nameList.length);
+    return nameList[i];
+}
+
+/* 5)
+Crea un array di numeri interi e fai la somma di tutti gli
+elementi che sono in posizione dispari */
+
+var numbersArray = [2,3,5,7,13,16,23,8,12,9,15];
+var sum = 0;
+
+for (var i = 0; i < numbersArray.length; i++) {
+    if(i % 2 != 0) {
+        sum += numbersArray[i];
+    }
+}
+
+console.warn("ESERCIZIO 5");
+console.log(sum);
+/* 6)
+Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro. */
+
+var oddNumber = [1,3,5,7,9,11,13,15,17,19];
+var evenNumber = [2,4,6,8,10,12];
+
+while (evenNumber.length < oddNumber.length) {
+    evenNumber.push(randomEvenNumber(14, 100));
+}
+
+console.warn("ESERCIZIO 6");
+console.log(oddNumber);
+console.log(evenNumber);
+
+function randomEvenNumber(min, max) {
+    var flag = false;
+    while (flag == false){
+        var evenNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        if (evenNumber % 2 == 0) {
+            return evenNumber;
+        }
+    }  
+}
+/* 7)
+Scrivi una funzione che fonda due array(aventi lo stesso
+numero di elementi) prendendo alternativamente gli
+elementi da uno e dall’altro
+es. [a, b, c], [1, 2, 3]→[a, 1, b, 2, c, 3]. */
+
+var letters = ["a", "b", "c", "d", "e"];
+var numbers = [1,2,3,4,5];
+var lettersNumbers = [];
+
+for (var i = 0; i < letters.length; i++) {
+    lettersNumbers.push(letters[i]);
+    lettersNumbers.push(numbers[i]);
+}
+
+console.warn("ESERCIZIO 7");
+console.log(lettersNumbers);
+
+/* 8)
+Scrivi una funzione che accetti tre argomenti:
+un array e due numeri(“a” più piccolo di “b” e “b” grande al
+massimo quanto il numero di elementi dell’array).
+La funzione ritornerà un nuovo array con i valori che
+hanno la posizione compresa tra “a” e “b” */
+
+var myArray = [1,2,3,4,5,6,7,8];
+
+console.warn("ESERCIZIO 8");
+console.log(myArray);
+console.log(newArray(myArray, 2, 5));
+
+function newArray(array, a, b) {
+    var newArray = [];
+    if ((a < b) && (b < array.length)) {
+        for (var i = a; i <= b; i++) {
+            newArray.push(array[i]);
+        }
+        return newArray;
+    } else {
+        return 0;
+    }
+}
+});
